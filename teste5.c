009315d49e375d3e2acfbeab4b160d15df1ec022
edenpsilva <pequenoeden@yahoo.com.br>
2014-03-06 22:19:24 -0200

    Create teste5.c

---
+++ teste5.c
@@ -0,0 +1,16 @@
+int a = 5;
+string b = "bla";
+
+void main(){
+	/*vou fazer um exemplo lexicamente correto, com if e while sÃ³ pra constar*/
+	if(b >= 2){
+		fat(b);
+	}else{
+		printf("%d", b);
+	}
+	while(a > 1){
+		a = a - 1;
+	}
+	return;
+}
+/*Ã€ medida que eu for vendo os erros nos programas de vocÃªs eu vou criando novos testes*/
